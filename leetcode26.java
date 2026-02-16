2987b8cf89214eb4e984034b8e5eab488b7fe3e1
Ariful Islam Rafid <arif.rafid14@gmail.com>
2026-02-16 23:07:14 +0600

    Refactor removeDuplicate method to remove duplicates

--- leetcode26.java
+++ leetcode26.java
@@ -1,38 +1,27 @@
-import java.util.Arrays;
-public class leetcode26{
+import java.util.Arrays; 
+public class leetcode26 {
 
-    public static int removeDuplicate(int [] nums){
-        int count=1;
-        int p=0;
-        for(int i=0; i<nums.length; i++){
-
-          if(count<nums.length){
-
-            for(int j =count; j<nums.length-i; j++){
-                if(nums[i]==nums[j]){
-                    
-                    int num = nums[j];
-                    for(int k=count; k<nums.length; k++){
-                        if(k<nums.length-1){
-                            nums[k]=nums[k+1];
-                        }
-                        
-                    }
-                    nums[nums.length-1]=num;
-                    p++;
-                }
-                
-            }
-            count++;
-          }
-
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0){
+            return 0;
         }
-        return p;
+
+        int k = 1; 
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
     }
-    public static void main(String [] args){
-        int [] nums = {0,0,1,1,1,2,2,3,3,4};
-        int p = removeDuplicate(nums);
-        System.out.println(p);
-        System.out.println(Arrays.toString (nums));
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,2,3,3,3,3,4,4,4,8,8,9,9};
+        int k = removeDuplicates(nums);
+
+        System.out.println(k);
+        System.out.println(Arrays.toString(nums));
     }
 }
